f6834cf2885a41ad4b622cd6fc37658e7f8f6382
Kayra Sarı <154630938+kyrasarii@users.noreply.github.com>
2025-07-19 19:50:41 +0300

    updated installed packages

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 cd
 mkdir Documents Downloads Pictures Videos Projects Clones
 cd Clones
-sudo pacman -S awesome brightnessctl dunst firefox fish fisher git grim htop hyprland hyprpaper hyprshot kitty neovim nwg-look playerctl polkit-kde-agent qt5-wayland qt5ct ranger sddm slurp spotify-player ttf-fira-code ttf-nerd-fonts-symbols uwsm vim wget wofi
+sudo pacman -S awesome brightnessctl dunst firefox fish fisher git grim htop hyprland hyprpaper hyprshot kitty neovim nwg-look playerctl polkit-kde-agent qt5-wayland qt5ct ranger sddm slurp spotify-player ttf-fira-code ttf-nerd-fonts-symbols uwsm vim wget wofi lazygit cmake meson ninja scons python python-pip python-setuptools perl ruby go rust clang llvm lld lldb valgrind git subversion mercurial wget curl openssh rsync gdb strace ltrace perf sysstat autoconf automake libtool devtools neovim nano tmux jq unzip zip xz p7zip htop man-db man-pages the_silver_searcher ripgrep
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
@@ -14,8 +14,5 @@ fish_config theme save "Catppuccin Mocha"
 
 cd
 cp -r ~/dotfiles/.config/* ~/.config/
-
-git clone https://github.com/LazyVim/starter ~/.config/nvim
-rm -rf ~/.config/nvim/.git
 
 chsh -s /bin/fish
